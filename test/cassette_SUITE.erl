540fe21b0b0e0e66d60fd54219d5467827366ef0
macpie <pierre@helium.com>
2018-05-09 16:25:00 -0700

    HELIUM-182: Improve `cassette` suite

--- test/cassette_SUITE.erl
+++ test/cassette_SUITE.erl
@@ -1,4 +1,4 @@
--module(intercept_SUITE).
+-module(cassette_SUITE).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
@@ -11,7 +11,7 @@
 ]).
 
 -export([
-    basic/1
+    udp/1
 ]).
 
 %%--------------------------------------------------------------------
@@ -25,7 +25,7 @@
 %% @end
 %%--------------------------------------------------------------------
 all() ->
-    [{group, intercept}].
+    [{group, cassette}].
 
 %%--------------------------------------------------------------------
 %% @public
@@ -34,7 +34,7 @@ all() ->
 %% @end
 %%--------------------------------------------------------------------
 groups() ->
-    [{intercept, [], [basic]}].
+    [{cassette, [], [udp]}].
 
 
 %%--------------------------------------------------------------------
@@ -66,16 +66,50 @@ end_per_suite(_Config) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
-basic(_Config) ->
+udp(_Config) ->
     ct:pal("START OF ~p", [?FUNCTION_NAME]),
-    ?assertEqual(ok, dummy:simple()),
+    intercept:add(gen_udp, gen_udp_intercepts, [{{send, 4}, send}]),
 
-    intercept:add(dummy, dummy_intercepts, [{{simple, 0}, simple}]),
+    _ = server(4000),
+    {ok, Socket} = gen_udp:open(0, [{active, true}, inet, binary]),
 
-    ?assertEqual({ok, intercepted}, dummy:simple()),
-    ?assertEqual(ok, intercept:clean(dummy)),
-    ?assertEqual(ok, dummy:simple()).
+    Message = <<"udp message">>,
+    ?assertEqual(ok, gen_udp:send(Socket, "127.0.0.1", 4000, Message)),
+    receive
+        {udp, _, _, _, <<"ok">>} -> ok
+    after 2000 ->
+        ct:fail("timeout")
+    end,
+
+    ?assertEqual(ok, gen_udp:send(Socket, "127.0.0.1", 4000, Message)),
+    receive
+        {udp, _, _, _, <<"ok">>} -> ok
+    after 2000 ->
+        ct:fail("timeout")
+    end,
+
+    ?assertEqual(ok, intercept:clean(gen_udp)),
+
+    ?assertEqual(ok, gen_udp:send(Socket, "127.0.0.1", 4000, Message)),
+    receive
+        {udp, _, _, _, <<"ok">>} -> ok
+    after 2000 ->
+        ct:fail("timeout")
+    end.
+
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+server(Port) ->
+    erlang:spawn(fun() ->
+        {ok, Socket} = gen_udp:open(Port, [binary]),
+        server_loop(Socket)
+    end).
+
+server_loop(Socket) ->
+    receive
+        {udp, Socket, Host, Port, _Bin} = Msg ->
+            gen_udp:send(Socket, Host, Port, <<"ok">>),
+            server_loop(Socket)
+    end.
--- test/gen_udp_intercepts.erl
+++ test/gen_udp_intercepts.erl
@@ -5,24 +5,23 @@
 ]).
 
 -define(MODULE_ORG, gen_udp_orig).
--define(R, "RECORDING").
--define(ENV, "RECORD").
 
 send(Socket, Ip, Port, Msg) ->
-    case os:getenv(?R, "false") of
-        "false" ->
-            case erlang:get(?R) of
-                undefined ->
-                    self() ! {udp, Socket, Ip, Port, Msg};
-                {Socket2, Ip2, Port2, Msg2} ->
-                    self() ! {udp, Socket2, Ip2, Port2, Msg2}
-            end;
-        "true" ->
+    case cache(Msg) of
+        {Socket2, Ip2, Port2, Msg2} ->
+            self() ! {udp, Socket2, Ip2, Port2, Msg2};
+        undefined ->
             ok = ?MODULE_ORG:send_orig(Socket, Ip, Port, Msg),
             receive
                 {udp, Socket2, Ip2, Port2, Msg2} ->
-                    erlang:put(?R, {Socket2, Ip2, Port2, Msg2}),
+                    cache(Msg, {Socket2, Ip2, Port2, Msg2}),
                     self() ! {udp, Socket2, Ip2, Port2, Msg2}
             end
     end,
     ok.
+
+cache(Key) ->
+    erlang:get(Key).
+
+cache(Key, Value) ->
+    erlang:put(Key, Value).
